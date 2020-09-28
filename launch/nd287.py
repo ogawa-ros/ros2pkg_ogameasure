471edf45626ebad9ecd5118ab22a6062b4e3a285
Ayu Konishi <63437641+ayu-nico@users.noreply.github.com>
2020-09-28 22:51:27 +0900

    Update nd287.py

--- launch/nd287.py
+++ launch/nd287.py
@@ -1,1 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ros2pkg_ogameasure',
+            node_executable='nd287',
+        )],
+       )
 
