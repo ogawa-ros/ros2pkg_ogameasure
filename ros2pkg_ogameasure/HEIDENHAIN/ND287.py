4232e4d6261c05f072f0ed280df28f3c9158d3d8
Ayu Konishi <63437641+ayu-nico@users.noreply.github.com>
2020-09-29 00:57:59 +0900

    Update ND287.py

--- ros2pkg_ogameasure/HEIDENHAIN/ND287.py
+++ ros2pkg_ogameasure/HEIDENHAIN/ND287.py
@@ -16,22 +16,17 @@ class ND287(object):
         self.node.declare_parameter("port") #いらない?
         az_port = self.node.get_parameter("~az_usbport").get_parameter_value().string_value
         el_port = self.node.get_parameter("~el_usbport").get_parameter_value().string_value
-        #az_port = rospy.get_param("~az_usbport")
-        #el_port = rospy.get_param("~el_usbport")
 
         self.encorder_az = ogameasure.HEIDENHAIN.ND287(az_port)
         self.encorder_el = ogameasure.HEIDENHAIN.ND287(el_port)
 
         topic_name_az = '/dev/'+node_name+'/'+az_port+'/'
-        topic_name_az = topic_name.replace('_','/').replace('.','_')
+        topic_name_az = topic_name_az.replace('_','/').replace('.','_')
         topic_name_el = '/dev/'+node_name+'/'+el_port+'/'
-        topic_name_el = topic_name.replace('_','/').replace('.','_')
+        topic_name_el = topic_name_el.replace('_','/').replace('.','_')
 
-        #Publisher
-        # self.pub_az = rospy.Publisher("/dev/ND287/__port__/az", Float64, queue_size=1)
-        # self.pub_el = rospy.Publisher("/dev/ND287/__port__/el", Float64, queue_size=1)
-        self.pub_az = rclpy.node.create_publisher(Float64, topic_name, 1)
-        self.pub_el = rclpy.node.create_publisher(Float64, topic_name, 1)
+        self.pub_az = rclpy.node.create_publisher(Float64, topic_name_az, 1)
+        self.pub_el = rclpy.node.create_publisher(Float64, topic_name_el, 1)
         self.az = self.get_az()
 
 
@@ -49,7 +44,7 @@ class ND287(object):
         return el
 
     def publish_el(self):
-        while not rospy.is_shutdown():
+        while not rclpy.is_shutdown():
             el = self.get_el()
             self.pub_el.publish(float(el))
             time.sleep(0.01)
@@ -57,7 +52,7 @@ class ND287(object):
 
     def publish_az(self):
         count = 0
-        while not rospy.is_shutdown():
+        while not rclpy.is_shutdown():
             az = self.az
             az2  = self.get_az()
             hensa = az2-az
